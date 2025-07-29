1a53688386e5d5e61057b3d2a6f709553d09ef5e
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2025-07-29 18:58:31 -0400

    feat: add verbose output

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
 load_dotenv()
 
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 def main():
-    validate_arguments()
-    user_prompt = get_user_prompt()
+    api_key = get_api_key()
+    client = genai.Client(api_key=api_key)
+
+    args = retrieve_arguments()
 
     messages = [
         types.Content(role="user", parts=[
-            types.Part(text=user_prompt)
+            types.Part(text=args.prompt),
         ])
     ]
 
@@ -26,19 +27,40 @@ def main():
 
     print(response.text)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    os._exit(0)
+    sys.exit(0)
 
+
+def get_api_key():
+    if not os.environ.get("GEMINI_API_KEY"):
+        print("API Key not provided")
+        sys.exit(1)
+
+    return os.environ.get("GEMINI_API_KEY")
 
 def validate_arguments():
     if len(sys.argv) < 2:
         print("Usage: python main.py <prompt>")
-        os._exit(1)
+        sys.exit(1)
 
 def get_user_prompt():
     return sys.argv[1]
+
+
+def retrieve_arguments():
+    validate_arguments()
+
+    parser = argparse.ArgumentParser()
+    # positional argument
+    parser.add_argument("prompt")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose print with some statistics")
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
